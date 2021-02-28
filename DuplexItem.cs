bd98ef81e2611e56255b5311e97311d1fd31ddf2
Victorya01 <76602663+Victorya01@users.noreply.github.com>
2021-03-01 02:25:52 +0300

    Create DuplexItem.cs

---
+++ DuplexItem.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace проба4.Model
+{
+    public class DuplexItem<T>
+    {
+        public T Data { get; set; }
+        public DuplexItem<T> Previous { get; set; }
+        public DuplexItem<T> Next { get; set; }
+
+
+        public DuplexItem(T data)
+        {
+            Data = data;
+        }
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
